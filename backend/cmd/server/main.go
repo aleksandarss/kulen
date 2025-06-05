3527427002ae73ebb0fc97e7d75b0fd77906f117
aleksandarss <aleksandar-sasa.calic@hotmail.com>
2025-06-05 19:15:47 +0200

    feat: adjust cors configuration for testing. Adjust helm chart and default values.

--- backend/cmd/server/main.go
+++ backend/cmd/server/main.go
@@ -29,7 +29,7 @@ func main() {
 
 	// cors setup
 	router.Use(cors.New(cors.Config{
-		AllowOrigins:     []string{"http://localhost:5173"},
+		AllowOrigins:     []string{"*"}, //[]string{"http://localhost:5173"},
 		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
 		AllowHeaders:     []string{"Origin", "Content-Type", "Authorization"},
 		AllowCredentials: true,
