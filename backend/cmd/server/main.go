51e2ec1b52b79ff978f9fc142b204ca1873f14dc
aleksandarss <aleksandar-sasa.calic@hotmail.com>
2025-06-11 12:54:37 +0200

    fix: multiple fixes for building frontend for production and adjusting helm chart.

--- backend/cmd/server/main.go
+++ backend/cmd/server/main.go
@@ -29,7 +29,7 @@ func main() {
 
 	// cors setup
 	router.Use(cors.New(cors.Config{
-		AllowOrigins:     []string{"http://localhost:5173", "https://kulen.mithrandir.calic.cloud"},
+		AllowOrigins:     []string{"*"},
 		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
 		AllowHeaders:     []string{"Origin", "Content-Type", "Authorization"},
 		AllowCredentials: true,
